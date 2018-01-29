9dbc6c8877ea12babab44139ee0909b35dc1375a
Myron <34206615+IamJoker03@users.noreply.github.com>
2018-01-29 20:09:40 +0530

    Delete hals.sh

--- hals.sh
+++
@@ -1,7 +0,0 @@
-#HAL's
-rm -rf hardware/qcom/display-caf
-git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b staging/lineage-15.1-caf-8996 hardware/qcom/display-caf/msm8996 
-rm -rf hardware/qcom/audio-caf 
-git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b staging/lineage-15.1-caf-8996 hardware/qcom/audio-caf/msm8996
-rm -rf hardware/qcom/media-caf 
-git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b staging/lineage-15.1-caf-8996 hardware/qcom/media-caf/msm8996
