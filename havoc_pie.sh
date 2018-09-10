f1804c66b4ea47726617f045cc83ba5062a68a85
Sagar Rokade <33886845+sagarrokade006@users.noreply.github.com>
2018-09-11 01:07:57 +0530

    Create havoc_pie.sh

---
+++ havoc_pie.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/TheScarastic/android_device_xiaomi_mido -b lineage-16.0 device/xiaomi/mido
+git clone https://github.com/TheScarastic/android_device_xiaomi_msm8953-common -b lineage-16.0 device/xiaomi/msm8953-common
+git clone https://github.com/TheScarastic/proprietary_vendor_xiaomi -b lineage-16.0 vendor/xiaomi
+git clone https://github.com/penglezos/android_kernel_xiaomi_msm8953 -b lineage-15.1 kernel/xiaomi/msm8953
+
+#HALS
+git clone https://github.com/nitrogen-project/android_hardware_qcom_display-caf-msm8996 -b p hardware/qcom/display-caf-msm8996
+git clone https://github.com/nitrogen-project/android_hardware_qcom_media-caf-msm8996 -b p hardware/qcom/media-caf-msm8996
+git clone https://github.com/nitrogen-project/android_hardware_qcom_audio-caf-msm8996 -b p hardware/qcom/audio-caf-msm8996
+
